5459307685e528440fe953b37a6b7a1d636ff521
ggarcia23 <gagarcia3@bsu.edu>
2021-09-21 16:38:37 -0400

    added a test into WikipediaRevisionParserTest to test to find the username, and created a class WikipediaUsernameParser that parses and finds the username

--- src/test/java/edu/bsu/cs222/WikipediaRevisionParserTest.java
+++ src/test/java/edu/bsu/cs222/WikipediaRevisionParserTest.java
@@ -15,4 +15,12 @@ public class WikipediaRevisionParserTest {
         String timestamp =  parser.parse(testDataStream);
         Assertions.assertEquals("2021-09-19T01:09:23Z", timestamp);
     }
+
+    @Test
+    public void testFindName() throws IOException {
+        WikipediaUsernameParser parser = new WikipediaUsernameParser();
+        InputStream testDataStream =  Thread.currentThread().getContextClassLoader().getResourceAsStream("test.json");
+        String user = parser.parse(testDataStream);
+        Assertions.assertEquals("MusikBot II", user);
+    }
 }
