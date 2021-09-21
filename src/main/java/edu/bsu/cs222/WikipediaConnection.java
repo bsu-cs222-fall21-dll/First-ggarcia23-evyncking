f25645d47cbb7bbb89261be466015ceea28a904b
evyncking <ecking@bsu.edu>
2021-09-21 16:39:04 -0400

    Added URL connection code to class

--- src/main/java/edu/bsu/cs222/WikipediaConnection.java
+++ src/main/java/edu/bsu/cs222/WikipediaConnection.java
@@ -1,5 +1,9 @@
 package edu.bsu.cs222;
 
 public class WikipediaConnection {
-
+        URL url = new URL("https://en.wikipedia.org");
+        URLConnection connection = url.openConnection();
+        connection.setRequestProperty("User-Agent",
+            "Revision Reporter/0.1 (ecking@bsu.edu; gagarcia3@bsu.edu)");
+        InputStream inputStream = connection.getInputStream();
 }
