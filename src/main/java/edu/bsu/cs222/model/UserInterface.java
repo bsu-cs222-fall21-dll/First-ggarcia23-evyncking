2a5db9f1d57d9534e4952463d8227ae88fc798d2
evyncking <ecking@bsu.edu>
2021-09-27 16:13:17 -0400

    Added three more classes

---
+++ src/main/java/edu/bsu/cs222/model/UserInterface.java
@@ -0,0 +1,13 @@
+package edu.bsu.cs222.model;
+
+public class UserInterface {
+    public String WikiUI() {
+
+        return String.format("Search Wikipedia for Article Page: ");
+
+    }
+
+    public void printMessage(String message){
+        System.out.println(message);
+    }
+}
