f44d22b0d74cb7123092f11add1f48340fcded6c
ggarcia23 <gagarcia3@bsu.edu>
2021-09-27 21:46:45 -0400

    changed into proper formatting

--- src/main/java/edu/bsu/cs222/model/Model/RevisionFormatter.java
+++ src/main/java/edu/bsu/cs222/model/Model/RevisionFormatter.java
@@ -2,7 +2,7 @@ package edu.bsu.cs222.model.Model;
 
 public class RevisionFormatter {
     public String revisionFormat(WikipediaRevision revisionToBeFormatted) {
-        return String.format("Timestamp: %s Username: %s%n",
+        return String.format("%s %s%n",
                 revisionToBeFormatted.getTimeStamp(), revisionToBeFormatted.getUser());
     }
 }
