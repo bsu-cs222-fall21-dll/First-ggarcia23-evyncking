17db6c01515285f92aaba408f23c1536c7db7f77
ggarcia23 <gagarcia3@bsu.edu>
2021-10-01 17:54:56 -0400

    added method which converts date and time to UTC

--- src/main/java/edu/bsu/cs222/model/Model/RevisionFormatter.java
+++ src/main/java/edu/bsu/cs222/model/Model/RevisionFormatter.java
@@ -4,7 +4,16 @@ package edu.bsu.cs222.model.Model;
 public class RevisionFormatter {
     public String revisionFormat(WikipediaRevision revisionToBeFormatted) {
         return String.format("%s %s%n",
-                revisionToBeFormatted.getTimeStamp(), revisionToBeFormatted.getUser());
+                UTCTimestampConverter(revisionToBeFormatted.getTimeStamp()), revisionToBeFormatted.getUser());
+    }
+
+    public String UTCTimestampConverter(String timestamp) {
+        String year = timestamp.substring(0,4);
+        String month = timestamp.substring(5,7);
+        String day = timestamp.substring(8,10);
+        String time = timestamp.substring(11,19);
+
+        return String.format("%s/%s/%s %s", month, day, year, time);
     }
 
 }
