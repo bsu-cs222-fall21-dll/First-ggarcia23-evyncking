87fe53633362cc55dbf018c79e2f45da056c6d86
ggarcia23 <gagarcia3@bsu.edu>
2021-10-01 21:28:56 -0400

    initial GUI setup

---
+++ src/main/java/edu/bsu/cs222/model/Controller/WikipediaGUI.java
@@ -0,0 +1,35 @@
+package edu.bsu.cs222.model.Controller;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class WikipediaGUI extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        VBox parent = new VBox();
+        parent.getChildren().add(new Label("ONS Wikipedia Revision Parser"));
+
+        HBox urlArea = new HBox(new Label("Article Name: "));
+        urlArea.setAlignment(Pos.BASELINE_CENTER);
+        TextField textField = new TextField();
+        urlArea.getChildren().add(textField);
+        parent.getChildren().add(urlArea);
+
+        Button button = new Button("Search Wikipedia");
+        button.setOnAction(event -> System.out.println("I would access a class" + textField.getText() + " here"));
+        parent.getChildren().add(button);
+
+        primaryStage.setTitle("ONS Wikipedia Revision");
+        primaryStage.setScene(new Scene(parent, 350, 300));
+        primaryStage.show();
+    }
+}
