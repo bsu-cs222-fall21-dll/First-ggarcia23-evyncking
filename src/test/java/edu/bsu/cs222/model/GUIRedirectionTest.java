7f7038099a31cf2faf1bb4a82db12cfeeac34680
evyncking <ecking@bsu.edu>
2021-10-04 17:07:54 -0400

    Added revisionTest to test class

--- src/test/java/edu/bsu/cs222/model/GUIRedirectionTest.java
+++ src/test/java/edu/bsu/cs222/model/GUIRedirectionTest.java
@@ -20,7 +20,6 @@ public class GUIRedirectionTest {
         JSONArray jsonArray = inputParser.parseJSON(sampleFileInputStream);
         String testRedirectInfo = userInterfaceFormatterTest.getRedirectInfo(testInput());
 
-
         String redirectionString = "Redirected to Frank Zappa";
 
         Assertions.assertEquals(redirectionString, testRedirectInfo);
@@ -30,6 +29,26 @@ public class GUIRedirectionTest {
         InputParser inputParser = new InputParser();
         InputStream sampleFileInputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("test.json");
         return inputParser.parseJSON(sampleFileInputStream);
+    }
+
+    @Test
+    public void revisionsTest() throws IOException {
+        InputStream sampleFileInputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("test.json");
+        InputParser inputParser = new InputParser();
+        UserInterfaceFormatter userInterfaceFormatterTest = new UserInterfaceFormatter();
+
+        JSONArray jsonArray = inputParser.parseJSON(sampleFileInputStream);
+
+        String testRevision = userInterfaceFormatterTest.formatRevisions(testInput());
+        Assertions.assertEquals(revisionStringTest(), testRevision);
+
+    }
+
+    private String revisionStringTest() {
+        return String.format("09/19/2021 01:09:23 75.172.213.218%n" +
+                "09/19/2021 00:54:29 75.172.213.218%n" +
+                "09/18/2021 23:53:23 MusikBot II%n" +
+                "09/18/2021 12:33:00 Anon423%n");
     }
 
 }
